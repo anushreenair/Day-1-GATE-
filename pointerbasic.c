81806d6bbc1cd24e84e5216ab978348c7e783c24
Anushree Nair <97722918+anushreenair@users.noreply.github.com>
2025-06-22 11:14:01 +0530

    Add files via upload

---
+++ pointerbasic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main (){
+    int a = 5;
+    int *p = &a; // Pointer to integer a
+
+    printf("%d\n", a);
+    printf("%d\n", p); // Print value pointed by p to use the variable
+    printf("%d\n", &a); // Print address of a
+    printf("%d\n", &p); // Print address stored in pointer p
+    printf("%d\n", *p); // Print address of a using pointer p
+    printf("%d\n", *p++);// Post-increment pointer p, but print value before increment
+    printf("%d\n",(*p)++);// Post-increment value pointed by p, but print value before increment
+    printf("%d\n", *p = a + 5); // Assign value to pointer p, which is a + 5
+
+    return 0;
+}
